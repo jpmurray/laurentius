5a59924e8820f9f1127826b5a4478ba958f5ca01
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2019-08-24 09:12:22 -0400

    added attribute to get binominal name of species

--- app/Species.php
+++ app/Species.php
@@ -8,6 +8,11 @@ class Species extends Model
 {
     protected $fillable = ['name', 'name_en', 'name_fr'];
 
+    public function getBinominalNameAttribute()
+    {
+        return $this->genus->name . " " . strtolower($this->name);
+    }
+
     public function genus()
     {
         return $this->belongsTo(Genus::class);
