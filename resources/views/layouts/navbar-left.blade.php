0c6d31b7dbbc361f3dc641e5d7068bd6b569165a
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2019-08-23 22:45:55 -0400

    Menu entries to manage taxonomies

--- resources/views/layouts/navbar-left.blade.php
+++ resources/views/layouts/navbar-left.blade.php
@@ -1,4 +1,34 @@
 <!-- Left Side Of Navbar -->
 <ul class="navbar-nav mr-auto">
-	
+	<a class="nav-link" href="{{ route("home") }}">Dashboard</a>
+	<li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            Manage taxonomies	 <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">
+            	Regnums
+            </a>
+            <a class="dropdown-item" href="#">
+            	Divisios
+            </a>
+            <a class="dropdown-item" href="#">
+            	Classes
+            </a>
+            <a class="dropdown-item" href="#">
+            	Ordos
+            </a>
+            <a class="dropdown-item" href="#">
+            	Familias
+            </a>
+            <a class="dropdown-item" href="#">
+            	Genera
+            </a>
+            <a class="dropdown-item" href="#">
+            	Species
+            </a>
+        </div>
+    </li>
+    {{-- <a class="nav-link" href="{{ route("settings") }}">Settings</a> --}}
 </ul>
