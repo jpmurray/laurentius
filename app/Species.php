f9e36977d3260a5379710c105fdf6d1f04776c2b
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2019-08-25 16:46:00 -0400

    Prepping for Media Library

--- app/Species.php
+++ app/Species.php
@@ -4,10 +4,13 @@ namespace App;
 
 use App\Traits\Species\Icons;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Species extends Model
+class Species extends Model implements HasMedia
 {
     use Icons;
+    use HasMediaTrait;
     
     const HARDINESS_CA = ['unknown', '0', '0a', '0b', '1', '1a', '1b', '2', '2a', '2b', '3', '3a', '3b', '4', '4a', '4b', '5', '5a', '5b', '6', '6a', '6b', '7', '7a', '7b', '8', '8a', '8b', '9', '9a', '9b'];
     const SUN = ['unknown', 'full', 'partial', 'shade'];
